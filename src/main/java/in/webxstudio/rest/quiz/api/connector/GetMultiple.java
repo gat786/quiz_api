1a8a51ccd51e1117adfe0046d68f899b74707b4c
Ganesh Tiwari <ganesht049@gmail.com>
2018-10-07 20:54:56 +0530

    Update GetMultiple.java

--- src/main/java/in/webxstudio/rest/quiz/api/connector/GetMultiple.java
+++ src/main/java/in/webxstudio/rest/quiz/api/connector/GetMultiple.java
@@ -8,49 +8,59 @@ import java.util.Random;
 import in.webxstudio.rest.quiz.api.models.*;
 
 public class GetMultiple {
+			
+	public List<Multiple> getMultipleData(String table_name)
+	{
+		Connection connect = null;
+		ResultSet set;
 		
+		List<Multiple> data=new ArrayList<>();
 		
-		public List<Multiple> getMultipleData(String table_name)
+		try 
 		{
-			Connection connect = null;
-			ResultSet set;
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connect=DriverManager.getConnection("jdbc:mysql://"+DatabaseInfo.dbHostUrl+":3306/"+DatabaseInfo.dbName, DatabaseInfo.dbUsername,DatabaseInfo.dbPassword);
 			
-			List<Multiple> data=new ArrayList<>();
-			
-			try 
-			{
-				Class.forName("com.mysql.cj.jdbc.Driver");
-				connect=DriverManager.getConnection("jdbc:mysql://"+DatabaseInfo.dbHostUrl+":3306/"+DatabaseInfo.dbName, DatabaseInfo.dbUsername,DatabaseInfo.dbPassword);
-				//data=getData("science_easy_multiple");
-				Multiple dataRetrive;
-				List<Multiple> returnData=new ArrayList<>();
-				PreparedStatement statement=connect.prepareStatement("select * from "+table_name+";");
+			Multiple dataRetrive;
+			List<Multiple> returnData=new ArrayList<>();
+			int count=0;
+
+			PreparedStatement statement=connect.prepareStatement("select count(*) from "+table_name+";");
+			set=statement.executeQuery();
+
+			while(set.next()){
+				count=set.getInt(1);
+			}
+
+			System.out.println(count);
+
+			List<int> questionNumbers=randomGenerator(count);
+
+			for (int question:questionNumbers){
+				PreparedStatement statement=connect.prepareStatement("select * from "+table_name+" where id="+question+";");
 				set=statement.executeQuery();
-				while(set.next()) 
-				{
-					dataRetrive=new Multiple(set.getString(1),set.getString(2),set.getString(3),set.getString(4),set.getString(5));
+				while(set.next()){
+					dataRetrive=new Multiple(set.getString(2),set.getString(3),set.getString(4),set.getString(5),set.getString(6));
 					returnData.add(dataRetrive);
 				}
-				connect.close();
-				System.out.println("Data retrieved Successfully");
-				data=returnData;
 			}
-			catch(SQLException | ClassNotFoundException e) 
-			{
-				e.printStackTrace();
-			}
-			System.out.println(data);
-			List<Integer> questionsList=new ArrayList<>(); 
-			Random rand=new Random();
-			while (questionsList.size()<10) {
-				int number=rand.nextInt(data.size());
-				if(!questionsList.contains( number))
-					questionsList.add(number);
-			}
-			List<Multiple> dataReturnable=new ArrayList<>();
-			for(int a:questionsList) {
-				dataReturnable.add(data.get(a));
-			}
-			return dataReturnable;
+			connect.close();
 		}
+		catch(SQLException | ClassNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+		return dataRetrive;
+	}
+
+	List<int> randomGenerator(int maxNumber){
+		Random rand=new Random();
+		List<Integer> questionsList=new ArrayList<>(); 
+		while (questionsList.size()<10) {
+			int number=rand.nextInt(maxNumber);
+			if(!questionsList.contains( number))
+				questionsList.add(number);
+		}
+		return questionsList;
+	}
 }
