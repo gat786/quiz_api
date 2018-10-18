a76f3908a1ef42d7fdfac28c1ed053387584b711
Ganesh Tiwari <ganesht049@gmail.com>
2018-10-18 21:08:12 +0530

    Update PostMethod.java

--- src/main/java/in/webxstudio/rest/quiz/api/questions/PostMethod.java
+++ src/main/java/in/webxstudio/rest/quiz/api/questions/PostMethod.java
@@ -18,8 +18,7 @@ public class PostMethod {
 	public String postMultiple(Multiple getSuggestion) {
 		Multiple postedQuestion = getSuggestion;
 		SendMultiple sender= new SendMultiple();
-		sender.sendSuggestion(postedQuestion.getQuestion(), postedQuestion.getOption1(),  postedQuestion.getOption2(),  postedQuestion.getOption3(),  postedQuestion.getAnswer(),"science");
-		
+		sender.sendSuggestion(postedQuestion.getQuestion(), postedQuestion.getOption1(),  postedQuestion.getOption2(),  postedQuestion.getOption3(),  postedQuestion.getAnswer(),postedQuestion.getSubject());		
 		return "Post Works";
 	}
 	
