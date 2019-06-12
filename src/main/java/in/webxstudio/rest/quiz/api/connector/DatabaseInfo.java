a83e7f820d14bfa696f8f73f7006508bae071a43
Ganesh Tiwari <ganesht049@gmail.com>
2019-06-12 17:30:52 +0530

    Update DatabaseInfo.java

--- src/main/java/in/webxstudio/rest/quiz/api/connector/DatabaseInfo.java
+++ src/main/java/in/webxstudio/rest/quiz/api/connector/DatabaseInfo.java
@@ -2,7 +2,7 @@ package in.webxstudio.rest.quiz.api.connector;
 
 public class DatabaseInfo {
 	public static String dbName="trivia_db";
-	public static String dbPassword="hotMAIL123@";
-	public static String dbHostUrl="159.89.161.122";
+	public static String dbPassword="8796445698";
+	public static String dbHostUrl="104.208.163.227";
 	public static String dbUsername="ganesh";
 }
